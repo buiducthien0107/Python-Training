04c64b6c481b1050c177010f50c97e6fdd8cabfe
buiducthien0107 <buiducthien29802@gmail.com>
2024-09-06 12:01:06 +0700

    get acquainted print

---
+++ Làm quen với python.py
@@ -0,0 +1,10 @@
+#Lệnh in cơ bản
+s = "Hello world"
+print(s.upper())#viết hoa tất cả chữ cái 
+print(s.lower())#viết thường tất cả
+print(s.capitalize())#viết hoa chữ cái đầu dòng
+print(s.title())#viết hoa chữ cái đàu tiên trong từ
+
+age = 45 
+print(f"tôi đã {age}")
+print("tôi đã",age)
