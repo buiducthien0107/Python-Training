46540b82df409aba4ba5e9cc1ec575394a8c8f5b
buiducthien0107 <buiducthien29802@gmail.com>
2024-09-07 11:10:04 +0700

    Format a string and handle

---
+++ Format a string and handle.py
@@ -0,0 +1,18 @@
+working_hours = int(input("Please enter working hours : "))
+hourly_amount = int(input("Please enter Hourly amount : "))
+
+#Nối chuỗi bằng toán tử 
+print("working_hours : " , working_hours )
+print("hourly_amount : ", hourly_amount)
+#Chuyển đổi chuỗi, số nguyên và số thực
+age = int(5.5)
+print(type(age))
+#suy diễn câu bằng 
+"Thiện năm nay 22 tuổi"
+name ="{} năm nay {} tuổi"
+print(name.format("Thiện", 22))
+#Xử lý chuỗi cơ bản 
+"Hello, World!".lower()       # "hello, world!"
+"Hello, World!".upper()       # "HELLO, WORLD!"
+"Hello, World!".capitalize()  # "Hello, world!"
+"hello, world!".title()       # "Hello, World!"
