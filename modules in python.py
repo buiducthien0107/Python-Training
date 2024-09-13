fde4f71352799f73f65880c42a032e740071b3b3
buiducthien0107 <buiducthien29802@gmail.com>
2024-09-13 15:45:24 +0700

    modules in python

---
+++ modules in python.py
@@ -0,0 +1,14 @@
+import math
+from math import ceil
+from math import * #import tất cả mọi thứ
+
+
+a = 4.5
+print(ceil(a))
+print(math.floor(a))
+
+#build module
+def get_sum(a,b):
+    return a + b
+
+ 
