1000ddb946b3e2d61f1a32cd7ebecd99e3aa9014
buiducthien0107 <buiducthien29802@gmail.com>
2024-09-13 16:00:45 +0700

    Lamda in python

---
+++ Lamda in python.py
@@ -0,0 +1,7 @@
+#lambda arguments_list: expression
+#lambda là keyword bắt buộc.
+#arguments_list là các tham số truyền vào lambda.
+#expression là các biểu thức xử lý lambda.
+d = lambda a,b: a + b 
+d(5,9)
+print(d) #<function <lambda> at 0x000001DBCB15A200>
